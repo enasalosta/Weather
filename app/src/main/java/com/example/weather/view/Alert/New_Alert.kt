3d2daad864d7aba150bb5e910286e59724398db1
enasalosta <96724284+enasalosta@users.noreply.github.com>
2022-02-10 18:13:07 +0200

    default design

---
+++ app/src/main/java/com/example/weather/view/Alert/New_Alert.kt
@@ -0,0 +1,13 @@
+package com.example.weather.view.Alert
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weather.R
+
+class New_Alert : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_alert)
+    }
+}
---
+++ app/src/main/java/com/example/weather/view/Favorite/NewFavorite.kt
@@ -0,0 +1,12 @@
+package com.example.weather.view.Favorite
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weather.R
+
+class NewFavorite : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_favorite)
+    }
+}
